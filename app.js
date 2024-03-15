dd0f12ef7155d0bc7ed3ed5442166408a172d82a
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-03-15 17:17:49 +0200

    feat: updated language switch script

--- app.js
+++ app.js
@@ -1,81 +1,105 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('./data.json')
-      .then(response => response.json())
-      .then(data => {
+document.addEventListener("DOMContentLoaded", function () {
+  let currentLanguage = "en"; // За замовчуванням англійська мова
+  let activeSlideIndex = 0; // Індекс активного слайда
+
+  // Функція зміни мови
+  function downloadData(language) {
+    fetch(`./data/data-${language}.json`)
+      .then((response) => response.json())
+      .then((data) => {
         initializeSlider(data);
       })
-      .catch(error => console.error('Error loading data:', error));
-  
-    function initializeSlider(data) {
-      const sliderList = document.querySelector('.slider .list');
-      const thumbnailList = document.querySelector('.slider .thumbnail');
-  
-      data.forEach((item, index) => {
-        const slideItem = document.createElement('div');
-        slideItem.classList.add('item');
-        if (index === 0) slideItem.classList.add('active');
-        slideItem.innerHTML = `
-          <img src="${item.imageSrc}" alt="${item.title}" />
-          <div class="content">
-            <p>${item.category}</p>
-            <h2 class="title">${item.title}</h2>
-            <p class="description">${item.description}</p>
-          </div>
-        `;
-        sliderList.appendChild(slideItem);
-  
-        const thumbnailItem = document.createElement('div');
-        thumbnailItem.classList.add('item');
-        if (index === 0) thumbnailItem.classList.add('active');
-        thumbnailItem.innerHTML = `
-          <img src="${item.imageSrc}" />
-          <div class="content">${item.title}</div>
-        `;
-        thumbnailList.appendChild(thumbnailItem);
-      });
-  
-      const items = document.querySelectorAll(".slider .list .item");
-      const next = document.getElementById("next");
-      const prev = document.getElementById("prev");
-      const thumbnails = document.querySelectorAll(".thumbnail .item");
-  
-      let itemActive = 0;
-  
-      next.onclick = function () {
-        itemActive = (itemActive + 1) % data.length;
+      .catch((error) => console.error("Error loading data:", error));
+  }
+
+  // Функція ініціалізації слайдера
+  function initializeSlider(data) {
+    const sliderList = document.querySelector(".slider .list");
+    const thumbnailList = document.querySelector(".slider .thumbnail");
+
+    // Очищення списків слайдів та мініатюр
+    sliderList.innerHTML = "";
+    thumbnailList.innerHTML = "";
+
+    data.forEach((item, index) => {
+      const slideItem = document.createElement("div");
+      slideItem.classList.add("item");
+      if (index === activeSlideIndex) slideItem.classList.add("active");
+      slideItem.innerHTML = `
+            <img src="${item.imageSrc}" alt="${item.title}" />
+            <div class="content">
+              <p>${item.category}</p>
+              <h2 class="title">${item.title}</h2>
+              <p class="description">${item.description}</p>
+            </div>
+          `;
+      sliderList.appendChild(slideItem);
+
+      const thumbnailItem = document.createElement("div");
+      thumbnailItem.classList.add("item");
+      if (index === activeSlideIndex) thumbnailItem.classList.add("active");
+      thumbnailItem.innerHTML = `
+            <img src="${item.imageSrc}" />
+            <div class="content">${item.title}</div>
+          `;
+      thumbnailList.appendChild(thumbnailItem);
+    });
+
+    // Оновлення подій кліків на наступний та попередній слайди
+    const items = document.querySelectorAll(".slider .list .item");
+    const next = document.getElementById("next");
+    const prev = document.getElementById("prev");
+    const thumbnails = document.querySelectorAll(".thumbnail .item");
+
+    next.onclick = function () {
+      activeSlideIndex = (activeSlideIndex + 1) % data.length;
+      showSlider();
+    };
+
+    prev.onclick = function () {
+      activeSlideIndex = (activeSlideIndex + data.length - 1) % data.length;
+      showSlider();
+    };
+
+    thumbnails.forEach((thumbnail, index) => {
+      thumbnail.addEventListener("click", () => {
+        activeSlideIndex = index;
         showSlider();
-      };
-  
-      prev.onclick = function () {
-        itemActive = (itemActive + data.length - 1) % data.length;
-        showSlider();
-      };
-  
-      thumbnails.forEach((thumbnail, index) => {
-        thumbnail.addEventListener("click", () => {
-          itemActive = index;
-          showSlider();
-        });
       });
-  
-      let refreshInterval = setInterval(() => {
+    });
+
+    let refreshInterval = setInterval(() => {
+      next.click();
+    }, 3000);
+
+    // Функція показу активного слайда
+    function showSlider() {
+      // Видалення активного класу зі старого активного слайда
+      document
+        .querySelector(".slider .list .item.active")
+        .classList.remove("active");
+      document
+        .querySelector(".thumbnail .item.active")
+        .classList.remove("active");
+
+      // Додавання активного класу до нового активного слайда
+      items[activeSlideIndex].classList.add("active");
+      thumbnails[activeSlideIndex].classList.add("active");
+      //clear auto time run slider
+      clearInterval(refreshInterval);
+      refreshInterval = setInterval(() => {
         next.click();
-      }, 8 * 1000);
-  
-      function showSlider() {
-        let itemActiveOld = document.querySelector(".slider .list .item.active");
-        let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
-        itemActiveOld.classList.remove("active");
-        thumbnailActiveOld.classList.remove("active");
-  
-        items[itemActive].classList.add("active");
-        thumbnails[itemActive].classList.add("active");
-  
-        clearInterval(refreshInterval);
-        refreshInterval = setInterval(() => {
-          next.click();
-        }, 8 * 1000);
-      }
+      }, 6000);
     }
-  });
-  
+  }
+
+  // Ініціалізація слайдера при завантаженні сторінки
+  downloadData(currentLanguage);
+
+  // Обробник події зміни мови
+  let chng = document.getElementById("change-lang");
+  chng.onclick = function () {
+    currentLanguage = currentLanguage === "en" ? "uk" : "en";
+    downloadData(currentLanguage);
+  };
+});
